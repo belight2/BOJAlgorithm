25fd6b3fccb04cf66208ad250472dc4c49b1fb5b
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-04-01 22:15:09 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+#define pb push_back
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+using namespace std;
+const char nl = '\n';
+void solve(){
+  ll a, b;
+  cin >> a >> b;
+  cout << a + b << nl;
+}
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while(t--) solve();
+}
