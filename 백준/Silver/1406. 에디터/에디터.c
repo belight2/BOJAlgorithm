f8a1ee21317da37fdc420e4268167de359d5d0f8
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-04-17 00:47:07 +0900

    [Silver II] Title: 에디터, Time: 104 ms, Memory: 19860 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.c
+++ 백준/Silver/1406. 에디터/에디터.c
@@ -1,101 +1,101 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+// 연결리스트의 기본 단위 : Node
 typedef struct Node{
   char data;
   struct Node *next;
   struct Node *prev;
-}node;
+} node;
 
+// 연결리스트의 기점과 종점, 크기
 node *head = NULL;
 node *tail = NULL;
 int size = 0;
-void init(char *s, int len){
-  head = (node*)malloc(sizeof(node));
-  head->data = '\0'; head->next = NULL; head->prev = NULL;
-  tail = (node*)malloc(sizeof(node));
-  tail->data = '\0'; tail->next = NULL; tail->prev = head;
-  head->next = tail;
 
-  if(len == 0) return;
-  node *cursor = tail;
-  for(int i = 0; i < len; i++) add(cursor, s[i]);
+void init(){
+  tail = (node*)malloc(sizeof(node));
+  tail->data = '\0';
+  tail->next = NULL;
+  tail->prev = NULL;
+  head = tail;
 }
-void add(node *cursor, char data){
-  if(size++ == 0){
-    head->data = data;
-    return;
-  }
+
+void add(node *cur, char data){
   node *new_node = (node*)malloc(sizeof(node));
   new_node->data = data;
-  new_node->next = cursor;
-  if(cursor == head){
+  new_node->next = cur;
+  size++;
+  if(cur == head){
     new_node->prev = NULL;
-    cursor->prev = new_node;
+    cur->prev = new_node;
     head = new_node;
     return;
   }
-  new_node->prev = cursor->prev;
-  cursor->prev->next = new_node;
-  cursor->prev = new_node;
+  cur->prev->next = new_node;
+  new_node->prev = cur->prev;
+  cur->prev = new_node;
 }
-void erase(node *cursor){
-  if(cursor == head || size == 0) return;
+
+void erase(node *cur){
+  if(cur == head || size == 0) return;
+  node *erase_node = cur->prev;
   size--;
-  node *erase_node = cursor->prev;
   if(erase_node == head){
-    head = cursor;
+    cur->prev = NULL;
+    head = cur;
     free(erase_node);
     return;
   }
-  erase_node->prev->next = cursor;
-  cursor->prev = erase_node->prev;
+  erase_node->prev->next = cur;
+  cur->prev = erase_node->prev;
   free(erase_node);
 }
 
+void print_list(){
+  node *cur = head;
+  if(cur == NULL) return;
+  while(cur->next != NULL){
+    printf("%c", cur->data);
+    cur = cur->next;
+  }
+  printf("\n");
+}
+
 void destroy(){
-  node *cursor = head;
-  while(cursor != NULL){
-    node *temp = cursor;
-    cursor = cursor->next;
+  node *cur = head;
+  while(cur != NULL){
+    node *temp = cur;
+    cur = cur->next;
     free(temp);
   }
   head = NULL;
   tail = NULL;
-  size = 0;
 }
-void print_node(){
-  if(size == 0) return;
-  node *cursor = head;
-  while(cursor != tail){
-    printf("%c", cursor->data);
-    cursor = cursor->next;
-  }
-  printf("\n");
-}
+
 int main(){
   char str[100005], op, x;
   int t;
   scanf("%[^\n]s", &str);
-  init(str, strlen(str));
   scanf("%d", &t);
+
+  init(); //연결리스트 초기화
+
   node *cursor = tail;
+  int len = strlen(str);
+  for(int i = 0; i < len; i++) add(cursor, str[i]);
+  
   while(t--){
     scanf(" %c", &op);
     if(op == 'P'){
       scanf(" %c", &x);
       add(cursor, x);
     }
-    else if(op == 'L'){
-      if(cursor != head) cursor = cursor->prev;
-    }
-    else if(op == 'B'){
-      erase(cursor);
-    }
-    else if(op == 'D'){
-      if(cursor != tail) cursor = cursor->next;
-    }
+    else if(op == 'B') erase(cursor);
+    else if(op == 'L' && cursor->prev != NULL) cursor = cursor->prev;
+    else if(op == 'D' && cursor->next != NULL) cursor = cursor->next;
   }
-  print_node();
-  destroy();
+  print_list();
+  // destroy(); // 연결리스트 동적해제
 }
