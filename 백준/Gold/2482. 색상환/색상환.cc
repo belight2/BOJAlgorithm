ad5b5d6abf8017c2ee6c3e70f58d04dab1c490a6
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-07-14 21:55:34 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 5964 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define X first
+#define Y second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define MOD 1'000'000'003
+const char nl = '\n';
+int n, k;
+int d[1005][1005];
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin >> n >> k;
+  for(int i = 0; i <= n; i++){
+    d[i][0] = 1;
+    d[i][1] = i;
+  }
+  for(int i = 4; i <= n; i++)
+    for(int j = 2; j <= k; j++)
+      d[i][j] = (d[i-2][j-1] + d[i-1][j]) % MOD;
+  cout << d[n][k];
+}
