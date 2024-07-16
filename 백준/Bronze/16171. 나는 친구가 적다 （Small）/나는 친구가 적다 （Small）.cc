feb3890d370d5d401b3424aea151de0343fa1455
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-07-17 03:55:37 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.cc
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.cc
@@ -1,19 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
   cin.tie(nullptr) -> sync_with_stdio(false);
   string s{}, k{}, a{};
   cin >> s >> k;
   for(auto it = s.begin(); it < s.end(); it++){
     if(isalpha(*it)) a.push_back(*it);
-  }
+  } 
   
   int len = k.size();
-  if(len == a.size()){
-    cout << ( a == k );
-    return 0; 
-  }
   bool ans{};
   for(auto it = a.begin(); it < a.end() - len + 1; it++){
     if(ans = (string(it, it+len) == k), ans) break;
