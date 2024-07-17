0392b67796c6aae46439b8c67850d9031638773f
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-07-18 05:05:32 +0900

    [Silver III] Title: The Big Dance, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6012. The Big Dance/The Big Dance.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+int n;
+ll solve(int st, int en){
+  if(en - st < 2) return 0;
+  if(en - st == 2) return st*(st+1);  
+  int mid = (st + en) / 2 + ((st+en)&1);
+  return solve(st, mid) + solve(mid, en);
+}
+int main() {
+  fastio(nullptr, false);
+  cin >> n;
+  cout << solve(1, n+1);
+}
