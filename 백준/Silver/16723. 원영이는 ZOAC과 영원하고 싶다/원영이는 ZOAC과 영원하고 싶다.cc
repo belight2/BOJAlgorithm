f1d98fe45b054ea13909623575e7c58df844ce64
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-07-18 09:25:50 +0900

    [Silver I] Title: 원영이는 ZOAC과 영원하고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16723. 원영이는 ZOAC과 영원하고 싶다/원영이는 ZOAC과 영원하고 싶다.cc
+++ 백준/Silver/16723. 원영이는 ZOAC과 영원하고 싶다/원영이는 ZOAC과 영원하고 싶다.cc
@@ -3,12 +3,11 @@ using namespace std;
 using ll = long long;
 int n;
 int main(){
-  cin.tie(0)->sync_with_stdio(0);
-  cin >> n;
+  scanf("%d", &n);
   ll ret{};
   for(int i = 0; (1 << i) <= n; i++){
     int a = (n+(1<<i)), b = (1 << (i+1));
     ret +=  a / b * b;
   }
-  cout << ret;
+  printf("%lld", ret);
 }
