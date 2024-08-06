24419760830aee986a6b68ad19f4afa07b7687c8
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-06 21:06:44 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,34 +1,24 @@
-#include <iostream>
-using namespace std;
-int a[1005];
-int tmp[1005];
-int n;
+#include <bits/stdc++.h>
 
-void merge(int st, int en){
-	int mid = (st+en)/2;
-	int lidx = st;
-	int ridx = mid;
-	for(int i = st; i < en; i++){
-		if( lidx >= mid ) tmp[i] = a[ridx++];
-		else if( ridx >= en ) tmp[i] = a[lidx++];
-		else if( a[lidx] > a[ridx] ) tmp[i] = a[ridx++];
-		else tmp[i] = a[lidx++];
-	}
-	for(int i = st; i < en; i++ ) a[i] = tmp[i];
-}
-void merge_sort(int st, int en){
-	if( en == st + 1 ) return;
-	int mid = (st+en)/2;
-	merge_sort(st, mid);
-	merge_sort(mid, en);
-	merge(st, en);
-}
+using namespace std;
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n;
-	for(int i = 0; i < n; i++ ) cin >> a[i];
-	merge_sort(0, n);
-	for(int i = 0; i < n; i++) cout << a[i] << '\n';
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  int n;
+
+  cin >> n;
+
+  vector<int> a(n);
+  for(auto &x : a) cin >> x;
+
+  // bubble sort
+  for(int i = 0; i < n-1; i++){
+    for(int j = 0; j < n - i - 1; j++){
+      if(a[j] > a[j+1]) 
+        swap(a[j], a[j+1]);
+    }
+  }
+
+  for(auto &x : a) cout << x << '\n';
 }
