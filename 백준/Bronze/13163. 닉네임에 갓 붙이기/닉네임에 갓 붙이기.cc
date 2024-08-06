2c8dd2898e5f6e5b744c8ac0cc786739a3791a03
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-07 00:25:11 +0900

    [Bronze II] Title: 닉네임에 갓 붙이기, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/13163. 닉네임에 갓 붙이기/닉네임에 갓 붙이기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+vs ans;
+void split(string str, char del){
+  stringstream ss(str);
+  string buffer;
+  while(getline(ss, buffer, del)) ans.pb(buffer);
+}
+int main() {
+  fastio(nullptr, false);
+  int t;
+  string str;
+  cin >> t;
+  cin.ignore();
+  while(t--){
+    vs {}.swap(ans);
+    getline(cin, str);
+    split(str, ' ');
+    ans[0] = "god";
+    for(auto &s : ans) cout << s;
+    cout << nl;
+  }
+}
