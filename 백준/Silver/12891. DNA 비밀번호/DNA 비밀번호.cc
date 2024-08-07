1324225480ba3296c2a6b4a89c7e134da9d89b86
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-07 14:14:47 +0900

    [Silver II] Title: DNA 비밀번호, Time: 16 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -1,33 +1,91 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int checkArr[4];
+int myArr[4];
+int checkSecret = 0;
+void Add(char c);
+void Remove(char c);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int slen, plen; // |S|, |P|
-int cnt[26];
-string DNA;
+    int S, P;
+    cin >> S >> P;
+    int Result = 0;
+    string A;
+    cin >> A;
+    for (int i = 0; i < 4; i++) {
+        cin >> checkArr[i];
+        if (checkArr[i] == 0)
+            checkSecret++;
+    }
+    for (int i = 0; i < P; i++) { //초기 P부분 문자열 처리 부분
+        Add(A[i]);
+    }
+    if (checkSecret == 4)
+        Result++;
+    // 슬라이딩 윈도우 처리 부분
+    for (int i = P; i < S; i++) {
+        int j = i - P;
+        Add(A[i]);
+        Remove(A[j]);
+        if (checkSecret == 4)  // 4자리 수에 대한 크기가 모두 충족되었을 때는 유효한 비밀번호
+            Result++;
+    }
+    cout << Result << "\n";
+}
 
-bool is_password(){ return (cnt['A'-'A'] <= 0)&&(cnt['C'-'A'] <= 0)&&(cnt['G'-'A'] <= 0)&&(cnt['T'-'A'] <= 0); }
-int main(){
-  cin.tie(nullptr)->sync_with_stdio(false);
+void Add(char c) { //새로 들어온 문자를 처리해주는 함수
+    switch (c) {
+    case 'A':
+        myArr[0]++;
+        if (myArr[0] == checkArr[0])
+            checkSecret++;
+        break;
+    case 'C':
+        myArr[1]++;
+        if (myArr[1] == checkArr[1])
+            checkSecret++;
+        break;
+    case 'G':
+        myArr[2]++;
+        if (myArr[2] == checkArr[2])
+            checkSecret++;
+        break;
+    case 'T':
+        myArr[3]++;
+        if (myArr[3] == checkArr[3])
+            checkSecret++;
+        break;
+    }
+}
 
-  cin >> slen >> plen;
-  cin >> DNA;  
-  cin >> cnt['A'-'A'] >> cnt['C'-'A'] >> cnt['G'-'A'] >> cnt['T'-'A'];
-
-  int ans{};
-  deque<int> dq;
-
-  for(int i = 0; i < slen; i++){
-    dq.push_back(i);
-    cnt[DNA[i] - 'A']--;
-    
-    if(dq.size() < plen) continue;
-    if(is_password()) ans++;
-
-    char cur = DNA[dq.front()];
-    dq.pop_front();
-    cnt[cur-'A']++;
-  }
-  cout << ans;
+void Remove(char c) { //제거되는  문자를 처리해주는 함수
+    switch (c) {
+    case 'A':
+        if (myArr[0] == checkArr[0])
+            checkSecret--;
+        myArr[0]--;
+        break;
+    case 'C':
+        if (myArr[1] == checkArr[1])
+            checkSecret--;
+        myArr[1]--;
+        break;
+    case 'G':
+        if (myArr[2] == checkArr[2])
+            checkSecret--;
+        myArr[2]--;
+        break;
+    case 'T':
+        if (myArr[3] == checkArr[3])
+            checkSecret--;
+        myArr[3]--;
+        break;
+    }
 }
