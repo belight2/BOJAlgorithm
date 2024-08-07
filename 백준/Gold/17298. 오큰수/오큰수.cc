168225911891e76e216f96a010dbe1ac01bd5734
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-07 15:27:04 +0900

    [Gold IV] Title: 오큰수, Time: 180 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using pi = pair<int, int>;
+#define X first
+#define Y second
+
+int n;
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  cin >> n;
+
+  vector<int> ans(n, -1);
+  stack<pi> stk;
+  int cur;
+  
+  cin >> cur;
+  stk.push({cur, 0});
+
+  for(int i = 1; i < n; i++){
+    cin >> cur;
+    while(stk.size() && stk.top().X < cur){
+      ans[stk.top().Y] = cur;
+      stk.pop();
+    }
+    stk.push({cur, i});
+  }
+
+  for(auto &x : ans) cout << x << ' ';  
+}
