da94cd1592cf6d534ce2a936b7cd119552dc6312
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-10 22:30:26 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 100 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+const int MOD = 1e9 + 7;
+
+ll tree[2'000'005];
+int n, m, k;
+
+void update(int p, ll val){
+  p += n;
+  tree[p] = val;
+  for(int i = p; i > 1; i>>=1) tree[i>>1] = (tree[i] * tree[i^1]) % MOD;
+}
+
+ll query(int l, int r){
+  ll ret = 1;
+  for( l += n, r += n; l < r; l>>=1, r>>=1 ){
+    if(l&1) ret = (ret * tree[l++]) % MOD;
+    if(r&1) ret = (ret * tree[--r]) % MOD;
+  }
+  return ret;
+}
+
+int main() {
+  fastio(nullptr, false);
+  
+  cin >> n >> m >> k;
+  
+  for(int i = 0; i < n; i++) cin >> tree[i+n];
+  for(int i = n-1; i > 0; --i) tree[i] = (tree[i<<1] * tree[i<<1|1])%MOD;
+
+  m += k;
+  while(m--){
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if(a&1) update(b-1, c);
+    else cout << query(b-1, c) << nl;
+  }
+}
