1fdef0312b8ea8f49bdeea171149dbcabca4467f
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-16 19:32:28 +0900

    [Gold I] Title: K번째 수, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,30 +1,32 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-using ll = long long;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-const char nl = '\n';
-
-ll n, k;
-
-bool solve(ll mid){
-  ll cnt{};
-  for(ll i = 1; i <= n; i++){
-    cnt += min(mid / i, n);
-  }
-  return cnt < k;
-}
-
-int main(){
-  cin.tie(nullptr)->sync_with_stdio(false);
-  
-  cin >> n >> k;
-  ll st{}, en = n * n + 1;
-  while(st < en){
-    ll mid = (st + en) / 2;
-    if(solve(mid)) st = mid + 1;
-    else en = mid;
-  }
-  cout << en;
+    long N, K;
+    cin >> N >> K;
+    long start = 1, end = K;
+    long ans = 0;
+    // 이분 탐색 수행
+    while (start <= end) {
+        long middle = (start + end) / 2;
+        long cnt = 0;
+        // 중앙 값보다 작은 수는 몇 개인지 계산.
+        for (int i = 1; i <= N; i++) {
+            cnt += min(middle/i, N);  // 작은 수를 카운트하는 핵심로직
+        }
+        if (cnt < K) {
+            start = middle + 1;
+        }
+        else {
+            ans = middle;  // 현재 단계의 중앙 값을 정답 변수에 저장
+            end = middle - 1;
+        }
+    }
+    cout << ans << "\n";
 }
