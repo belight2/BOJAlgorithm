3c8cd4831e8778314fdffd1dce2d78f92cff68e2
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-16 19:32:00 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -1,39 +1,45 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-const char nl = '\n';
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int n, m;
-vector<int> lec;
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    int start = 0;
+    int end = 0;
 
-bool solve(int th){
-  int cnt = 1, tot{}, mx{};
-  for(int i = 0; i < n; i++){
-    if(tot + lec[i] > th){
-      mx = max(mx, tot);
-      cnt++;
-      tot = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+        if (start < A[i]) {
+            start = A[i]; // 레슨 최대값을 시작인덱스로 저장
+        }
+        end = end + A[i]; // 모든 레슨의 총 합을 종료 인덱스로 저장
     }
-    tot += lec[i];
-  }
-  mx = max(mx, tot);
-  return cnt <= m && mx <= th;
-}
 
-int main(){
-  cin.tie(nullptr)->sync_with_stdio(false);
-  
-  cin >> n >> m;
-  lec.resize(n);
-  for(auto &cur : lec) cin >> cur;
-
-  int st = 0;
-  int en = 1e9+1;
-  while(st < en){
-    int mid = (st+en)/2;
-    if(solve(mid)) en = mid;
-    else st = mid+1;
-  }
-  cout << st;
+    while (start <= end) {
+        int middle = (start + end) / 2;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < N; i++) { // middle값으로 모든 레슨을 저장 할 수 있는지 확인
+            if (sum + A[i] > middle) {
+                count++;
+                sum = 0;
+            }
+            sum = sum + A[i];
+        }
+        if (sum != 0)
+            count++;
+        if (count > M)
+            start = middle + 1;
+        else
+            end = middle - 1;
+    }
+    cout << start << "\n";
 }
