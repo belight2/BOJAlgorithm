76367950e82637c523e7b5e06f23292cb81a187e
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-17 19:48:06 +0900

    [Gold III] Title: 나머지 합, Time: 132 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,45 +1,29 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pi;
-typedef pair<ll, ll> pl;
-typedef tuple<int, int, int> ti;
-typedef tuple<ll, ll, ll> tl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<ti> vti;
-typedef vector<tl> vtl;
-#define X first
-#define Y second
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-const char nl = '\n';
-int n;
-ll ans, m;
-vl a(1'000'000);
-vl freq(1000);
-int main() {
-  cin.tie(0)->sync_with_stdio(0);
+
+using ll = long long;
+
+ll aC2(ll a){ return a * (a-1) / 2; }
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+  
+  int n{}, m{};
   cin >> n >> m;
-  for(int i = 0; i < n; i++) cin >> a[i];
-//#define DEBUG_
-#ifndef DEBUG_
-  ll prefix = 0;
+
+  vector<ll> a(n+1);
+  vector<ll> b(m);
+
+  for(int i = 1; i <= n; i++) cin >> a[i];
+  
+  ll ans{}, sum{};
   for(int i = 1; i <= n; i++){
-    prefix = (prefix + a[i-1]) % m;
-    if(!prefix) ans++;
-    freq[prefix]++;
+    sum = (sum + a[i]) % m;
+    if(!sum) ans++;
+    b[sum]++;
   }
-  for(int i = 0; i < m; i++){
-    if(!freq[i]) continue;
-    ans += (freq[i] * (freq[i] - 1)) / 2;
-  }
+  for(int i = 0; i < m; i++) ans += aC2(b[i]);
+  
   cout << ans;
-#endif
-#ifdef DEBUG_
-#endif
 }
