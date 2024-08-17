f0f74ebaf033076e794a8d84aa417f0247ea62ec
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-17 19:44:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,19 +1,24 @@
+// Authored by : chjh2129
+// http://boj.kr/f6f804dcc7a9441a81e300ce998c964d
 #include <bits/stdc++.h>
+
 using namespace std;
+
+int n, m, a;
+
 int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  int n, m;
+  cin.tie(nullptr)->sync_with_stdio(false);
+
   cin >> n >> m;
-  vector<vector<int>> a(n+1, vector<int>(n+1));
+
   vector<vector<int>> s(n+1, vector<int>(n+1));
   for(int i = 1; i <= n; i++){
     for(int j = 1; j <= n; j++){
-      cin >> a[i][j];
-      s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + a[i][j];
+      cin >> a;
+      s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + a;
     }
   }
+
   while(m--){
     int sx, sy, ex, ey;
     cin >> sx >> sy >> ex >> ey;
