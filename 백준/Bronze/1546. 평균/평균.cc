33573ac3c8970fe2ed3523b2c86472a177a85e53
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-17 19:39:39 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,15 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
+#include <bits/stdc++.h>
+
 using namespace std;
+
+int n;
+vector<int> a;
+
 int main(){
   cin.tie(nullptr)->sync_with_stdio(false);
-  int n;
+  
   cin >> n;
-  vector<int> a(n);
+  a.resize(n);
   for(auto &cur : a) cin >> cur;
+
   int tot = accumulate(a.begin(), a.end(), 0);
   int mx = *max_element(a.begin(), a.end());
+  
   cout << double(tot * 100.0 / mx / n);
 }
