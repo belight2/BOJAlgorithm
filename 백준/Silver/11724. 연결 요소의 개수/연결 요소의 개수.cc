e46afb92a8a35b5d0f68c8a1177bad8b3c36e288
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-18 20:16:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 6536 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -3,34 +3,40 @@
 using namespace std;
 
 int n, m, u, v;
-vector<int> adj[1005];
-bool vis[1005];
+vector<int> adj[1001];
+vector<bool> vis(1001);
 
-void dfs(int cur){
-  vis[cur] = 1; 
-  for(auto nxt : adj[cur]){
-    if(vis[nxt]) continue;
-    dfs(nxt);
+void bfs(int st){
+  queue<int> q;
+  vis[st] = 1;
+  q.push(st);
+
+  while(q.size()){
+    auto cur = q.front(); q.pop();
+    for(auto nxt : adj[cur]){
+      if(vis[nxt]) continue;
+      vis[nxt] = 1;
+      q.push(nxt);
+    }
   }
 }
-
 int main(){
   cin.tie(nullptr)->sync_with_stdio(false);
   
   cin >> n >> m;
-
+  
   while(m--){
     cin >> u >> v;
     adj[u].push_back(v);
-    adj[v].push_back(u);  
+    adj[v].push_back(u);
   }
 
   int ans{};
   for(int i = 1; i <= n; i++){
     if(vis[i]) continue;
     ans++;
-    dfs(i);
+    bfs(i);
   }
-
+  
   cout << ans;
 }
