7639c30f63fcab22139ab7ff72128d5283bdbd09
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-18 22:13:55 +0900

    [Gold V] Title: ABCDE, Time: 16 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.cc
+++ 백준/Gold/13023. ABCDE/ABCDE.cc
@@ -1,38 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-const char nl = '\n';
-int n, m, u, v;
-vector<int> adj[2001];
-vector<bool> vis(2001);
+#include <bits/stdc++.h>
 
-void solve(int idx, int k){
-	if(k == 4){
-		cout << 1 << nl;
-		exit(0);	
-	}
-	for(auto nxt : adj[idx]){
-		if(!vis[nxt]){
-			vis[nxt] = 1;
-			solve(nxt, k+1);
-			vis[nxt] = 0;
-		}
-	}
+using namespace std;
+
+int n, m, a, b;
+vector<int> adj[2005];
+bool vis[2005];
+
+bool dfs(int cur, int dep){
+  if(dep == 4) return 1;
+  for(auto nxt : adj[cur]){
+    if(vis[nxt]) continue;
+    vis[nxt] = 1;
+    if(dfs(nxt, dep+1)) return 1;
+    vis[nxt] = 0;
+  }
+  return 0;
 }
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> m;
-	while(m--){
-		cin >> u >> v;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
-	for(int i = 0; i < n; i++){
-		vis[i] = 1;
-		solve(i, 0);
-		vis[i] = 0;
-	}
-	cout << 0 << nl;
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  cin >> n >> m;
+
+  while(m--){
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  bool ans{};
+  for(int i = 1; i <= n; i++){
+    vis[i] = 1;
+    if(ans = dfs(i, 0)) break;
+    vis[i] = 0;
+  }
+
+  cout << ans;
 }
