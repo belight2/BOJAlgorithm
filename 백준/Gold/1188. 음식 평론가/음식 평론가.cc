a63348283ea4bbba726919d25e9541b8b9d3c365
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-24 19:55:19 +0900

    [Gold IV] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+int gcd(int a, int b){ return (b ? gcd(b, a%b) : a); }
+
+int n, m;
+
+int main() {
+  fastio(nullptr, false);
+  cin >> n >> m;
+  cout << m - gcd(n, m);
+}
