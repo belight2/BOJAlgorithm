086e20395fb4163a3ea15dcce94b09e3e7202a3b
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-08-26 19:15:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -2,23 +2,22 @@
 
 using namespace std;
 
-int n, m, a;
+int n, m;
 
 int main(){
   cin.tie(nullptr)->sync_with_stdio(false);
-  
-  cin >> n >> m;
 
-  vector<int> prefix_sum(n+1);
+  cin >> n >> m;  
 
-  for(int i = 1; i <= n; i++){
-    cin >> a;
-    prefix_sum[i] = prefix_sum[i-1] + a;
-  }
+  vector<int> a(n+1);
+
+  for(int i = 1; i <= n; i++) cin >> a[i];
+
+  partial_sum(a.begin(), a.end(), a.begin());
 
   while(m--){
-    int sx, ex;
-    cin >> sx >> ex;
-    cout << prefix_sum[ex] - prefix_sum[sx - 1] << '\n';
+    int st, en;
+    cin >> st >> en;
+    cout << a[en] - a[st-1] << '\n';
   }
 }
