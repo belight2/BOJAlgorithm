21ec2c98ee249ed641a0e0a12543d6cc921f478d
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-09-05 18:30:52 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 96 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
@@ -6,7 +6,7 @@ int a[1000005];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    fill(a, a+1000005, -1);
+
     cin >> n >> k;
     for(int i = 0; i < n; i++) cin >> a[i];
     int res = 0;
