e2b8621023ba8eaaba3e08d0dcaeabef9036a2a7
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-09-06 19:18:49 +0900

    [Silver IV] Title: 제로, Time: 4 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,22 +1,32 @@
-#include <iostream>
-#include <stack>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
 using namespace std;
+
+// stack 사용 시 주의점
+// stack이 비어있는 상태에서 pop()이나 top()을 하게되면 런타임 에러가 발생합니다.
+// 그래서 항상, pop(), top() 명령어 수행 전 스택이 비어있는지 확인해야 합니다.
+
+int k, x;
+stack<int> stk;
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	stack<int> stk;
-	unsigned int res = 0;
-	int t, x;
-	cin >> t;
-	while(t--){
-		cin >> x;
-		if( x == 0 && !stk.empty() ) stk.pop();
-		else stk.push(x);
-	}
-	while(!stk.empty()){
-		res += stk.top();
-		stk.pop();
-	}
-	cout << res;
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input & init
+  cin >> k;
+  while(k--){
+    // 정수를 조건식에 넣을 경우, 0이 아닌 경우 모두 true, 0이면 false입니다.
+    if(cin >> x, x) stk.push(x);
+    else if(!stk.empty()) stk.pop();
+  }
+
+  // solve
+  int ans{};
+  while(!stk.empty()){
+    ans += stk.top();
+    stk.pop();
+  }
+
+  // output
+  cout << ans;
 }
