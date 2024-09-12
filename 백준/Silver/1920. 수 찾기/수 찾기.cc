21047de3d34522ffac2e09728362381f258be802
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-09-12 17:46:19 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,50 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+  
+  int n, m;
 
-    int N, M;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
+  cin >> n;
+  vector<int> a(n);
+  for(auto &x : a) cin >> x;
+  
+  sort(a.begin(), a.end());
 
-    sort(A.begin(), A.end()); //A배열 정렬 O(nlogn)시간 복잡도
-
-    cin >> M;
-    for (int i = 0; i < M; i++) {
-        bool find = false;
-        int target;
-        cin >> target;
-        // 이진 탐색 시작
-        int start = 0;
-        int end = A.size() - 1;
-        while (start <= end) {
-            int midi = (start + end) / 2;
-            int midV = A[midi];
-            if (midV > target) {
-                end = midi - 1;
-            }
-            else if (midV < target) {
-                start = midi + 1;
-            }
-            else {
-                find = true;
-                break;
-            }
-        }
-        if (find) {
-            cout << 1 << "\n";
-        }
-        else {
-            cout << 0 << "\n";
-        }
-    }
+  cin >> m;
+  while(m--){
+    int x;
+    cin >> x;
+    cout << binary_search(a.begin(), a.end(), x) << '\n';
+  }
 }
