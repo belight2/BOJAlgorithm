1d9685bb5c4f91ee924ba1808586d2be9af285d6
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-09-27 15:37:42 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 196 ms, Memory: 22248 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+int n;
+int a[1'000'005];
+int pos[1'000'005];
+vector<int> lis;
+
+int main() {
+  fastio(nullptr, false);
+
+  // input
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i];  
+
+  // solve
+  for(int i = 0; i < n; i++){
+    int idx = lower_bound(all(lis), a[i]) - lis.begin();
+    
+    if(idx == sz(lis)) lis.pb(a[i]);
+    else lis[idx] = a[i];
+    
+    pos[i] = idx;
+  }
+  
+  vi ans;
+  int cx = max_element(pos, pos+n) - pos;
+  int cy = pos[cx];
+  
+  ans.pb(a[cx]);
+  while(cy > 0){
+    for(int nx = cx; nx >= 0; nx--){
+      if(pos[nx] == cy - 1 && a[cx] > a[nx]){
+        ans.pb(a[nx]);
+        cy--;
+        cx = nx;
+        break;
+      }
+    }
+  }
+
+  // output
+  cout << sz(ans) << nl;
+  for(auto it = ans.rbegin(); it < ans.rend(); it++) cout << *it << ' ';
+}
