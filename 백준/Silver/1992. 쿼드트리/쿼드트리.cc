ab1aa38f85e3c081623dbcedcbaa375f28f2ac5d
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-09-30 19:55:13 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,39 +1,44 @@
-#include <iostream>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
 
 using namespace std;
-char map[66][66];
-bool check(int x, int y, int n){
-	for(int i = x; i < x + n; i++ )
-		for(int j = y; j < y+n; j++ )
-			if(map[x][y] != map[i][j])
-				return false;
-	return true;
+
+int n;
+string image[66];
+
+bool chk(int x, int y, int n){
+  for(int i = x; i < x + n; i++){
+    for(int j = y; j < y + n; j++){
+      if(image[x][y] != image[i][j])
+        return 0;
+    }
+  }
+  return 1;
 }
-void func(int x, int y, int n){
-	if(check(x,y,n)){
-		cout << map[x][y];
-		return;
-	}
-	if( n >= 2 ) cout << '(';
-	int slice = n / 2;
-	for(int i = 0; i < 2; i++){
-		for(int j = 0; j < 2; j++){
-			func(x+i*slice, y+j*slice, slice);
-		}
-	}
-	if( n >= 2 ) cout << ')';	
+
+void solve(int x, int y, int n){
+  if(chk(x, y, n)){
+    cout << image[x][y];
+    return;
+  }
+  cout << '(';
+  int nxt = n >> 1;
+  for(int i = 0; i < 2; i++){
+    for(int j = 0; j < 2; j++){
+      solve(x + i * nxt, y + j * nxt, nxt);
+    }
+  }
+  cout << ')';
 }
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	string str;
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> str;
-		for(int j = 0; j < n; j++)
-			map[i][j] = str[j];
-	}
-	func(0,0,n);
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> n;
+  for(int i = 0; i < n; i++)
+    cin >> image[i];
+  
+  // solve
+  solve(0, 0, n);
 }
