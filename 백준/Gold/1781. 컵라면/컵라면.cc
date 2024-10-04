b8a28f687f26f860227ad45e3dbfa180945316c1
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-04 22:46:34 +0900

    [Gold II] Title: 컵라면, Time: 60 ms, Memory: 9908 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,25 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-priority_queue<int> a;
-vector<vector<int>> deadLine(200'002);
-int N, x, y;
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> N;
-  for(int i = 0; i < N; i++){
+
+int n, x, y;
+priority_queue<int> pq;
+vector<vector<int>> dead_line(200'002);
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> n;
+  for(int i = 0; i < n; i++){
     cin >> x >> y;
-    deadLine[x].push_back(y);
+    dead_line[x].push_back(y);
   }
-  int ans = 0;
-  for(int cur = N; cur > 0; cur--){
-    for(int k : deadLine[cur]) a.push(k);
-    if(a.empty()) continue;
-    ans += a.top();
-    a.pop();
-  }
+
+  // solve
+  /*
+    모든 문제는 풀이하는데 단위 시간 1이 걸린다.
+    데드 라인을 역순으로 우선순위 큐에 문제 당 컵라면 개수를 삽입하여
+    데드 라인 전에 풀었을 때, 가장 많이 받을 수 있는 컵라면의 개수를 알 수 있다.
+  */
+  int ans{};
+  for(int cur = n; cur > 0; cur--){
+    for(int noodle : dead_line[cur]) pq.push(noodle);
+    if(pq.empty()) continue;
+    ans += pq.top();
+    pq.pop();
+  } 
+
+  // output
   cout << ans;
 }
