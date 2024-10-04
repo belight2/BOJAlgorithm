2845f2a669ce93a4ac70a759fa74c0caabb3a1e2
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-04 22:46:23 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4076 ms, Memory: 166248 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,30 +1,36 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
 #define ll long long
-using namespace std;
-vector<ll> x, y;
-ll a[4005], b[4005], c[4005], d[4005];
-int n;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> a[i] >> b[i] >> c[i] >> d[i];
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++){
-            x.push_back(a[i] + b[j]);
-            y.push_back(c[i] + d[j]);
-        }
-    sort(x.begin(), x.end());
-    sort(y.begin(), y.end());
-    ll res = 0;
-    for(ll k : x){
-        auto ub = upper_bound(y.begin(), y.end(), -k);
-        auto lb = lower_bound(y.begin(), y.end(), -k);
-        res += ub - lb;
-    }
-    cout << res;
 
+using namespace std;
+
+int n;
+vector<int> x, y;
+ll a[4005], b[4005], c[4005], d[4005];
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i] >> b[i] >> c[i] >> d[i];
+
+  // init
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      x.push_back(a[i]+b[j]);
+      y.push_back(c[i]+d[j]);
+    }
+  }
+  sort(x.begin(), x.end());
+  sort(y.begin(), y.end());
+
+  // solve
+  ll ans{};
+  for(int val : x){
+    ans += upper_bound(y.begin(), y.end(), -val) - lower_bound(y.begin(), y.end(), -val);
+  }
+
+  // output
+  cout << ans;
 }
