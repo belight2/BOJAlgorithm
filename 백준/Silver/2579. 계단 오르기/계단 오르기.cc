f70e72779d486eccf21603b38f2131a723a817e2
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-05 16:57:53 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+int arr[304];
+int DP[304][2];
+int res;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> arr[i];
+	}
+	//DP
+	DP[1][0] = arr[1];
+	DP[2][0] = arr[2];
+	DP[2][1] = arr[1] + arr[2];
+	for(int idx = 3; idx <= n; idx++){
+		DP[idx][0] = max(DP[idx-2][0], DP[idx-2][1]) + arr[idx];
+		DP[idx][1] = DP[idx-1][0] + arr[idx];
+	}
+	res = max(DP[n][0], DP[n][1]);
+	cout << res;
+}
