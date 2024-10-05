97798cd3d65ed381537582113044b47878767a30
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-05 16:48:42 +0900

    [Silver I] Title: 스타트링크, Time: 12 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int elev[1000002];
+int dist[1000002];
+int F, S, G, U, D;
+int dx[2];
+queue<int> Q;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> F >> S >> G >> U >> D;
+	dx[0] = U; dx[1] = -1*D;
+	Q.push(S);
+	dist[S]++;
+	while(!Q.empty()){
+		int cur = Q.front(); Q.pop();
+		for(int dir = 0; dir < 2; dir++){
+			int nx = cur + dx[dir];
+			if( nx < 1 || nx > F ) continue;
+			if(dist[nx] > 0) continue;
+			dist[nx] = dist[cur] + 1;
+			Q.push(nx);
+		}
+	}
+	if(dist[G] == 0) cout << "use the stairs";
+	else cout << dist[G] - 1;
+}
