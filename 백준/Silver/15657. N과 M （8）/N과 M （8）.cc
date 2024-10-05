ae1fd6c27f625c9a109570ca9840863cfaffa4f3
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-05 17:01:00 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[10];
+int num[10];
+int n, m;
+
+void func(int k, int idx){
+	if( k == m ){
+		for(int i = 0; i < m; i++ ) cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for(int i = idx; i < n; i++){
+		arr[k] = num[i];
+		func(k+1, i);
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for(int i = 0; i < n; i++ ) cin >> num[i];
+	sort(num, num+n);
+	func(0, 0);
+}
