f68da9e82e6b8665b80d8ccd5fb569b40edc7f28
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-05 17:06:14 +0900

    [Silver IV] Title: 문자열 집합, Time: 44 ms, Memory: 7828 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, ans;
+int n, m;
 unordered_map<string, bool> h;
-string str;
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     cin >> n >> m;
     while(n--){
+        string str;
         cin >> str;
         h[str] = 1;
     }
+    int ans{};
     while(m--){
+        string str;
         cin >> str;
         if(h.find(str) == h.end()) continue;
         ans++;
