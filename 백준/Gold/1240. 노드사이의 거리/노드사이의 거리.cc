2825f917b954de36c80bfcccc76bdc9d24f176d5
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-06 19:04:33 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 6120 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,39 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <algorithm>
-#include <tuple>
-#define X first
-#define Y second
+// Authored by : chjh2129
+#include <bits/stdc++.h>
+
 using namespace std;
-const char nl = '\n';
-vector<pair<int,int>> adj[1'000];
-int dist[1'005][1'005];
-int n, m, u, v, w;
-int root;
-void dfs(int cur, int prv){
-  int vertex, weight;
-  for(auto nxt : adj[cur]){
-    if(nxt.X == prv) continue;
-    dist[root][nxt.X] = dist[root][cur] + nxt.Y;
-    dfs(nxt.X, cur);
+
+using pi = pair<int, int>;
+
+int n, m;
+vector<pi> adj[1005];
+int dist[1005][1005];
+
+void dfs(int prv, int cur, int root){
+  for(auto &[nxt, weight] : adj[cur]){
+    if(nxt == prv) continue;
+    dist[root][nxt] = dist[root][cur] + weight;
+    dfs(cur, nxt, root);
   }
 }
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
   cin >> n >> m;
   for(int i = 0; i < n - 1; i++){
+    int u, v, w;
     cin >> u >> v >> w;
     adj[u].push_back({v, w});
     adj[v].push_back({u, w});
   }
-  for(int i = 1; i <= n; i++){
-   root = i;
-   dfs(i , -1); 
-  }
+
+  // solve
+  for(int i = 1; i <= n; i++) dfs(-1, i, i);
+  
+  // output
   while(m--){
+    int u, v;
     cin >> u >> v;
-    cout << dist[u][v] << nl;
+    cout << dist[u][v] << '\n';
   }
 }
