089d359ba16c811f04d3ccdfcd5cfc4f8f8df281
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-11 20:37:48 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,36 +1,40 @@
-#include <iostream>
-#include <algorithm>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
 
 using namespace std;
-int arr[10];
-int num[10];
-bool isUsed[10];
-int n, m;
 
-void func(int k){
-	if( k == m ){
-		for(int i = 0; i < m; i++ ) cout << arr[i] << ' ';
-		cout << '\n';
-		return;
-	}
-	int tmp = 0;
-	for(int i = 0; i < n; i++ ){
-		if(!isUsed[i]&& tmp != num[i]){
-			isUsed[i] = true;
-			arr[k] = num[i];
-			tmp = arr[k];
-			func(k+1);
-			isUsed[i] = false;
-		}
-	}
+int n, m;
+int num[10];
+bool isused[10];
+int a[10];
+
+void solve(int k){
+  if(k == m){
+    for(int i = 0; i < m; i++) cout << a[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  int tmp = 0;
+
+  for(int i = 0; i < n; i++){
+    if(isused[i] || tmp == num[i]) continue;
+    isused[i] = 1;
+    a[k] = num[i];
+    tmp = a[k];
+    solve(k+1);
+    isused[i] = 0;
+  }
 }
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m;
-	for(int i = 0; i < n; i++ ) cin >> num[i];
-	sort(num, num+n);
-	func(0);
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> n >> m;
+  for(int i = 0; i < n; i++) cin >> num[i];
+
+  // solve
+  sort(num, num+n);
+  solve(0);
 }
