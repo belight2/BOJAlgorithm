ddf2a98a791d89abfb6b7e042956aa11ac77b320
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-17 13:53:52 +0900

    [Gold III] Title: 중량제한, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.cc
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -1,60 +1,57 @@
+// Authored by : chjh2129
 #include <bits/stdc++.h>
+/*
+  간선 정보를 가중치가 큰 순서부터 순차적으로 merge를 진행하여 풀이합니다.
+
+  임의의 간선 (w, u, v)가 merge가 되고 출발지(st)와 목적지(en)가 같은 집합에 속하게되었다면.
+
+  이전에 연결된 간선의 가중치는 w보다 크거나 같습니다. 이후에 연결할 간선 정보는 w보다 작거나 같으므로,
+
+  출발지에서 목적지로 들고갈 수 있는 중량의 최댓값은 w가 됩니다.
+*/
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pi;
-typedef pair<ll, ll> pl;
-typedef tuple<int, int, int> ti;
-typedef tuple<ll, ll, ll> tl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<ti> vti;
-typedef vector<tl> vtl;
-#define X first
-#define Y second
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-const char nl = '\n';
-int n, m, s, e;
-vpl adj[10005];
-bool bfs(int limit){
-  vector<bool> vis(n+1);
-  queue<int> q;
-  q.push(s);
-  vis[s] = 1;
-  while(!q.empty()){
-    auto cur = q.front(); q.pop(); 
-    for(auto &[cost, nxt] : adj[cur]){
-      if( vis[nxt] || cost < limit) continue;
-      vis[nxt] = 1;
-      q.push(nxt);
-    }
-  }
-  return vis[e];
+using ti = tuple<int, int, int>;
+
+int n, m; // 정점과 간선의 개수
+vector<ti> edges; // 간선 정보
+
+vector<int> p(10005, -1);
+
+int find(int x){ return (p[x] < 0 ? x : p[x] = find(p[x])); }
+
+void merge(int x, int y){
+  x = find(x);
+  y = find(y);
+  if(x == y) return;
+  if(p[x] == p[y]) p[x]--;
+  if(p[x] > p[y]) swap(x, y);
+  p[y] = x;
 }
-int main() {
-  cin.tie(0)->sync_with_stdio(0);
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input  
   cin >> n >> m;
   while(m--){
-    ll u, v, c;
-    cin >> u >> v >> c;
-    adj[u].pb({c, v});
-    adj[v].pb({c, u});
+    int u, v, w;
+    cin >> u >> v >> w;
+    edges.push_back({w, u, v});
   }
-  cin >> s >> e;
-//#define DEBUG_
-#ifndef DEBUG_
-  int st = 0, en = 1e9+1;
-  while(st+1 < en){
-    int mid = (st+en)/2;
-    if(bfs(mid)) st = mid;
-    else en = mid;
+  int st, en; // 출발지와 목적지
+  cin >> st >> en;
+  
+  // solve
+  int ans{};
+  sort(edges.rbegin(), edges.rend()); // 내림차순 정렬
+  for(auto &[w, u, v] : edges){
+    merge(u, v);
+    if(find(st) == find(en)){
+      ans = w;
+      break;
+    }
   }
-  cout << st;
-#endif
-#ifdef DEBUG_
-#endif
+
+  // output
+  cout << ans;  
 }
