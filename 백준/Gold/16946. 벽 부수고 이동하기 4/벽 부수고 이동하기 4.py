55dee8ae99500b24b8c235395b2ebd55e58838a8
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-10-20 21:33:14 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 560 ms, Memory: 167264 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -1,7 +1,21 @@
 #Authored by : chjh2129
 import sys
 from collections import deque
+'''
+  flood fill 기법을 사용합니다.
 
+  너비우선탐색을 진행하면서 방문하는 점들을 모두 merge해줍니다.
+
+  union by size로 진행해 각 그룹마다 크기를 구해줍니다.
+
+  이중 for loop를 돌면서 벽이라면 인접한 4칸을 확인해 서로 다른 그룹인 경우 더한 값을 출력하면 됩니다.   
+
+  n * m 2차원 배열의 (x, y) 위치에 대해서 x * m + y로 계산하게되면 각 칸마다 유일한 값을 가집니다.
+
+  이를 노드의 번호를 가지고 union-find 하시면 됩니다.
+
+  이렇게 2차원 배열에서 특정 영역들을 분리하거나 크기가 필요할 때 union-find를 사용할 수 있다.
+'''
 input = sys.stdin.readline
 
 def find(x: int, p: list[int]) -> int:
