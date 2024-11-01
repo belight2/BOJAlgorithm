9dce096c86e63a3d871a05c5c30b1c7d8988fa0f
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-01 21:45:48 +0900

    [Gold III] Title: 웜홀, Time: 16 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,49 +1,65 @@
+// Authored by : chjh2129
 #include <bits/stdc++.h>
+/*
+  이 문제는 출발점이 정해져있지 않고 negative cycle이 있는지 묻고있는 문제입니다.
+
+  모든 정점을 출발점으로 보고 거리 테이블을 0으로 초기화한 상태로 벨만-포드를 시작합니다.
+
+  음수 가중치를 가진 간선 정보만 갱신이 됩니다.
+  
+  만약, negative cycle을 있다면 n번째 완화에서 거리 테이블이 갱신이 됩니다.
+*/
 using namespace std;
-typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
-typedef tuple<int,int,int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
-typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
-typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
-typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
-#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
-#define X first
-#define Y second
-#define pb push_back
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-const char nl = '\n';
+
+using ll = long long;
+using ti = tuple<int, int, int>;
+
+int n, m, w;
+
 void solve(){
-  vti edges;
-  int n, m, w;
+  // input & init 
   cin >> n >> m >> w;
-  vl dist(n+1);
+
+  // init
+  vector<ti> edges; // 간선 정보
+  vector<ll> d(n+1); // 거리 배열
+
+  // 간선 정보 
   while(m--){
     int s, e, t;
     cin >> s >> e >> t;
-    edges.pb({s, e, t});
-    edges.pb({e, s, t});
+
+    edges.push_back({s, e, t});
+    edges.push_back({e, s, t});
   }
+
+  // 웜홀 정보
   while(w--){
     int s, e, t;
     cin >> s >> e >> t;
-    edges.pb({s, e, -t});
+
+    edges.push_back({s, e, -t});
   }
+
   string ans = "NO";
+
+  // Bellman-Ford Algorithm
   for(int i = 0; i < n; i++){
-    for(auto &edge : edges){
-      auto &[s, e, t] = edge;
-      if(dist[s] + t < dist[e]){
-        dist[e] = dist[s] + t;
-        if(i == n-1) ans = "YES";
-      } 
+    for(auto &[s, e, t] : edges){
+      if(d[s] + t >= d[e]) continue;
+      
+      d[e] = d[s] + t;
+      if(i == n-1) ans = "YES"; // nth 완화에서 갱신이 발생
     }
   }
-  cout << ans << nl;
+
+  // output
+  cout << ans << '\n';
 }
-int main() {
-  fastio(nullptr, false);
-  int t{};
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int t;
   cin >> t;
   while(t--) solve();
 }
