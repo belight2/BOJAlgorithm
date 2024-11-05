3dff7caf6ed5fc055d451321ea3d4285cce821f6
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-05 15:14:50 +0900

    [Gold I] Title: 가계부 (Hard), Time: 48 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.cc
+++ 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.cc
@@ -1,44 +1,40 @@
+// Authored by : chjh2129
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
-typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
-typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
-typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
-typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
-#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
-#define X first
-#define Y second
-#define pb push_back
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-const char nl = '\n';
 
-int n, q;
+using namespace std;
+
+using ll = long long;
+
+int n, m;
 ll tree[2'000'005];
 
 void update(int p, ll val){
   p += n;
   tree[p] += val;
-  for(int i = p; i > 1; i>>=1) tree[i>>1] = tree[i] + tree[i^1];
+  for(int i = p; i > 1; i >>= 1) tree[i >> 1] = tree[i] + tree[i ^ 1];
 }
 
-ll query(int l, int r){
+ll sum_query(int l, int r){
   ll ret{};
-  for(l += n, r += n; l < r; l>>=1, r>>=1){
-    if(l&1) ret += tree[l++];
-    if(r&1) ret += tree[--r];
+  for(l += n, r += n; l < r; l >>= 1, r >>= 1){
+    if(l & 1) ret += tree[l++];
+    if(r & 1) ret += tree[--r];
   }
   return ret;
 }
 
-int main() {
-  fastio(nullptr, false);
-  cin >> n >> q; 
-  while(q--){
-    ll cmd, p, x;
-    cin >> cmd >> p >> x;
-    if(--cmd) cout << query(p-1, x) << nl;
-    else update(p-1, x);
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> n >> m;
+
+  // Query
+  while(m--){
+    ll cmd, a, b;
+    cin >> cmd >> a >> b;
+
+    if(cmd&1) update(a-1, b);
+    else cout << sum_query(a-1, b) << '\n';
   }
 }
