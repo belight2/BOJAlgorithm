f7876aebf75f5dd5a269709d637fb2b74a446517
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-12 02:30:37 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+    long a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << 56 * a + 24 * b + 14 * c + 6 * d;
+}
