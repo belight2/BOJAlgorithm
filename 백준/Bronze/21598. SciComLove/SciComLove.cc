96dac032fc74422ab94cbe5160f15cbf1e1c8bc6
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-13 00:43:10 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,16 @@
+// Authored by : chjh2129
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  cin >> n;
+
+  while(n--){
+    cout << "SciComLove\n";
+  }  
+}
