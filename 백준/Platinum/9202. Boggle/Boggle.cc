973a6fe917f54845e98467fce3e99abb8b004b0a
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-17 22:02:46 +0900

    [Platinum V] Title: Boggle, Time: 920 ms, Memory: 255152 KB -BaekjoonHub

--- 백준/Platinum/9202. Boggle/Boggle.cc
+++ 백준/Platinum/9202. Boggle/Boggle.cc
@@ -1,25 +1,7 @@
+// Authored by : chjh2129
 #include <bits/stdc++.h>
+
 using namespace std;
-typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
-typedef tuple<int,int,int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
-typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
-typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
-typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
-#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
-#define X first
-#define Y second
-#define pb push_back
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-const char nl = '\n';
-
-const int ROOT = 1;
-const int MX = 2'400'005;
-
-int unused = 2;
-int chk[MX];
-int nxt[MX][26];
 
 const int score[9] = { 0, 0, 0, 1, 1, 2, 3, 5, 11 };
 const int dx[8] = { 1, 1, 1, -1, -1, -1, 0, 0 };
@@ -29,31 +11,37 @@ string board[4];
 bool vis[4][4];
 int mark = 2;
 
-int sc;
-int cnt;
+int sc, cnt;
 string ans;
 
-int ctoi(char c){ return c - 'A'; }
+const int ROOT = 1;
+const int MX = 2'400'005;
+
+int unused = ROOT + 1;
+int chk[MX];
+int nxt[MX][26];
+
+int idx(char c){ return c - 'A'; }
 
 void insert(string &s){
   int cur = ROOT;
   for(auto c : s){
-    if(nxt[cur][ctoi(c)] == -1) nxt[cur][ctoi(c)] = unused++;
-    cur = nxt[cur][ctoi(c)];
+    if(nxt[cur][idx(c)] == -1) nxt[cur][idx(c)] = unused++;
+    cur = nxt[cur][idx(c)];
   }
   chk[cur] = 1;
 }
 
 void find(int cx, int cy, int cur, string s){
-  if(cur == -1 || sz(s) > 8) return;
+  if(cur == -1 || s.size() > 8) return;
   if(chk[cur] != 0 && chk[cur] != mark){
     chk[cur] = mark;
-    sc += score[sz(s)];
+    sc += score[s.size()];
     cnt++;
-    if(sz(ans) < sz(s)) ans = s;
-    else if(sz(ans) == sz(s) && s < ans) ans = s;
+    if(ans.size() < s.size()) ans = s;
+    else if(ans.size() == s.size() && s < ans) ans = s;
   }
-  
+
   for(int dir = 0; dir < 8; dir++){
     int nx = cx + dx[dir];
     int ny = cy + dy[dir];
@@ -61,28 +49,35 @@ void find(int cx, int cy, int cur, string s){
     if(vis[nx][ny]) continue;
     char c = board[nx][ny];
     vis[nx][ny] = 1;
-    find(nx, ny, nxt[cur][ctoi(c)], s + c);
+    find(nx, ny, nxt[cur][idx(c)], s + c);
     vis[nx][ny] = 0;
   }
 }
 
 void solve(){
-  sc = 0;
+  sc = cnt = 0;
   ans = "";
-  cnt = 0;
+
+  // 각 점을 시작 위치로 백트래킹
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 4; j++){
       vis[i][j] = 1;
-      find(i, j, nxt[ROOT][ctoi(board[i][j])], string(1, board[i][j]));
+      find(i, j, nxt[ROOT][idx(board[i][j])], string(1, board[i][j]));
       vis[i][j] = 0;
     }
   }
-  cout << sc << " " << ans << " " << cnt << nl; 
-  mark++;
+
+  cout << sc << ' ' << ans << ' ' << cnt << '\n';
+  mark++; // 다음 Boggle을 위해서 +1
 }
-int main() {
-  fastio(nullptr, false);
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // init
   memset(nxt, -1, sizeof(nxt));
+
+  // input
   int w, b;
   cin >> w;
   while(w--){
@@ -90,6 +85,7 @@ int main() {
     cin >> s;
     insert(s);
   }
+
   cin >> b;
   while(b--){
     for(int i = 0; i < 4; i++) cin >> board[i];
