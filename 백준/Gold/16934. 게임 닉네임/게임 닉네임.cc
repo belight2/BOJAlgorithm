ac8cc2bda3d18f408ecb1b4aa622b087898525a2
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-17 21:49:57 +0900

    [Gold III] Title: 게임 닉네임, Time: 84 ms, Memory: 107492 KB -BaekjoonHub

--- 백준/Gold/16934. 게임 닉네임/게임 닉네임.cc
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.cc
@@ -1,52 +1,57 @@
+// Authored by : chjh2129
 #include <bits/stdc++.h>
+
 using namespace std;
-typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
-typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
-typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
-typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
-typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
-#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
-#define X first
-#define Y second
-#define pb push_back
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-const char nl = '\n';
-namespace trie{
+
+// namespace 형태로 구현해보겠음
+
+namespace trie {
   const int root = 1;
   const int mx = 1'000'005;
-  int unused = 2;
+  int unused = root + 1;
   int chk[mx];
   int nxt[mx][26];
+
   void init(){ memset(nxt, -1, sizeof(nxt)); }
-  int ctoi(char c){ return c - 'a'; }
+
+  int idx(char c){ return c - 'a'; }
+
+  // 삽입을 하고, 별칭을 반환
   string insert(string &s){
     bool flag{};
     string ret{};
-    auto cur = root;
+    int cur = root;
+
     for(auto c : s){
-      if(nxt[cur][ctoi(c)] == -1){
-        nxt[cur][ctoi(c)] = unused++;
-        if(!flag) ret.pb(c);
+      if(nxt[cur][idx(c)] == -1){
+        nxt[cur][idx(c)] = unused++;
+        if(!flag) ret.push_back(c);
         flag = 1;
       }
-      if(!flag) ret.pb(c);
-      cur = nxt[cur][ctoi(c)];
+      if(!flag) ret.push_back(c);
+      cur = nxt[cur][idx(c)];
     }
+
     chk[cur]++;
     if(!flag && chk[cur] != 1) ret += to_string(chk[cur]);
+    
     return ret;
   }
 }
+
 int n;
-string str;
-int main() {
-  fastio(nullptr, false);
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // init
   trie::init();
+
   cin >> n;
   while(n--){
+    string str;
     cin >> str;
-    cout << trie::insert(str) << nl;
+
+    cout << trie::insert(str) << '\n';
   }
 }
