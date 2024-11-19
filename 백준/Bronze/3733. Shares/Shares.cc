149ef92b0e286f17317771fccc46350ad64152f9
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-19 15:18:44 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,12 @@
+// Authored by : chjh2129
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int n, s;
+  while(cin >> n >> s){
+    cout << s / (n+1) << '\n';
+  }
+}
