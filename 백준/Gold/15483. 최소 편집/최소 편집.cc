82294b28353ef49dc46c6b288237b97dcab61f7a
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2024-11-29 15:55:57 +0900

    [Gold III] Title: 최소 편집, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Gold/15483. 최소 편집/최소 편집.cc
+++ 백준/Gold/15483. 최소 편집/최소 편집.cc
@@ -1,31 +1,35 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+// Authored by : chjh2129
+#include <bits/stdc++.h>
+
 using namespace std;
-int dp[1005][1005];
+
+int n, m;
+string a, b;
+int d[1005][1005];
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	string a, b;
-	cin >> a >> b;
-	int len_a = a.length();
-	int len_b = b.length();
-	// 초기값 설정
-	for(int i = 0; i <= len_a; i++){
-		dp[i][0] = i; // 삭제연산  
-	} 
-	for(int j = 0; j <= len_b; j++){
-		dp[0][j] = j; //삽입 연산 
-	}
-	
-	for(int i = 1; i <= len_a; i++){
-		for(int j = 1; j <= len_b; j++){
-			if(a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1];
-			else{
-				dp[i][j] = 1 + min(min(dp[i-1][j], dp[i-1][j-1]), dp[i][j-1]);
-			}
-		}
-	}
-	cout << dp[len_a][len_b];
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  // input
+  cin >> a >> b;
+  
+  // init
+  n = a.size();
+  m = b.size();
+  for(int i = 1; i <= max(n, m); i++) d[i][0] =  d[0][i] = i;
+
+  // solve
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      if(a[i-1] == b[j-1]){
+        d[i][j] = d[i-1][j-1];
+        continue;
+      }
+
+      d[i][j] = 1 + min({d[i-1][j], d[i-1][j-1], d[i][j-1]});
+    }
+  }
+
+  // output
+  cout << d[n][m];
 }
