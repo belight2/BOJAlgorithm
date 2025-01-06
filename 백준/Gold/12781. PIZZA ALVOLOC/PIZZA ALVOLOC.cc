80eb315a6291c477125799029b0dafa98280afbc
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2025-01-06 17:12:30 +0900

    [Gold III] Title: PIZZA ALVOLOC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+template<typename T>
+class Point{
+    private:
+        T x, y;
+
+    public:
+        Point() {}
+        Point(T x, T y): x(x), y(y) { }
+
+        int ccw(const Point<T> &a, const Point<T> &b) {
+            T cross = (x * a.y + a.x * b.y + b.x * y) - (a.x * y + x * b.y + b.x * a.y);
+            if(!cross) return 0;
+            return (cross < 0 ? -1 : 1);
+        }
+
+        bool operator<=(const Point<T> &other) const {
+            return this -> x < other.x || (this -> x == other.x && this -> y <= other.y);
+        }
+};
+
+template<typename T>
+class Line{
+    private:
+        Point<T> a;
+        Point<T> b;
+        bool is_inner(const Point<T> &c) {
+            return a <= c && c <= b;
+        }
+
+    public:
+        Line() {}
+        Line(T ax, T ay, T bx, T by): a(ax, ay), b(bx, by) {
+            if(b <= a) {
+                swap(a, b);
+            }
+        }
+
+        int ccw(Point<T> c) {
+            return a.ccw(b, c);
+        }
+
+        bool is_intersection(Line other) {
+            T this_cross = 1, other_cross = 1;
+            for(int i = 0; i < 4; i++) {
+                T cur = (i < 2 ? ccw(i ? other.b : other.a) : other.ccw(i == 2 ? a : b));
+                // if(!cur && (i < 2 ? is_inner(i ? other.b : other.a) : other.is_inner(i == 2 ? a : b))) return 1;
+                (i < 2 ? this_cross : other_cross) *= cur;
+            }
+            return (this_cross < 0 && other_cross < 0);
+        }
+};
+
+int main() {
+    fastio(nullptr, false);
+
+    Line<int> a, b;
+    for(int i = 0; i < 2; i++) {
+        int sx, sy, ex, ey;
+        cin >> sx >> sy >> ex >> ey;
+        (i ? b : a) = Line<int>(sx, sy, ex, ey);
+    }
+
+    cout << a.is_intersection(b);
+}
