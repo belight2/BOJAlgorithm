7e980c3058219692d0b59ce447374f276f98d030
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2025-04-26 19:17:47 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 128 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+vi p(1'000'005);
+
+int find(int x) {
+    return (p[x] < 0 ? x : p[x] = find(p[x]));
+}
+
+void merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x == y) {
+        return;
+    }
+    if(--p[x] > p[y]) {
+        swap(x, y);
+    }
+    p[y] = x;
+}
+
+int n, m, k;
+
+void solve(int tc) {
+    cout << "Scenario " << tc << ":\n";
+    cin >> n >> m;
+    
+    fill(all(p), -1); 
+    while(m--) {
+        int u, v;        
+        cin >> u >> v;
+        merge(u, v);
+    }
+
+    cin >> k;
+    while(k--) {
+        int u, v;
+        cin >> u >> v;
+        cout << (find(u) == find(v)) << nl;
+    }
+    cout << nl;
+}
+
+int main() {
+    fastio(nullptr, false);
+    int tc{};
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) solve(i);
+}
