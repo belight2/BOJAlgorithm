3f4569605dacadfd9db67c752d565339e9f68da5
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2025-11-10 23:09:00 +0900

    [Silver II] Title: 키로거, Time: 240 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+
+void solve(int tc) {
+    string keyLog;
+    cin >> keyLog;
+
+    list<char> L{};
+    auto cursor = L.begin();
+    for(auto key : keyLog) {
+        if (key == '<') {
+            if(cursor != L.begin()) {
+                cursor--;
+            }
+        } else if(key == '>') {
+            if(cursor != L.end()) {
+                cursor++;
+            }
+        } else if(key == '-') {
+            if(cursor != L.begin()) {
+                cursor--;
+                cursor = L.erase(cursor);
+            }
+        } else {
+            L.insert(cursor, key);
+        }
+    }
+
+    for(auto x : L) {
+        cout << x;
+    }
+    cout << nl;
+}
+
+int main() {
+    fastio(nullptr, false);
+    int tc{};
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) solve(i);
+}
