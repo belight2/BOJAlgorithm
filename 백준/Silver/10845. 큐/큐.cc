02999aaea30da145c6eea66e42406ac15d4d2ec1
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2025-11-25 23:13:19 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,48 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef pair<int,int> pi; typedef pair<ll, ll> pl;
+typedef tuple<int, int, int> ti; typedef tuple<ll, ll, ll> tl; typedef vector<int> vi; typedef vector<ll> vl;
+typedef vector<pi> vpi; typedef vector<pl> vpl; typedef vector<ti> vti; typedef vector<tl> vtl;
+typedef vector<string> vs; typedef vector<bool> vb; typedef queue<int> qi; typedef queue<ll> ql;
+typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef queue<tl> qtl;
+#define fastio(x, y) cin.tie((x))->sync_with_stdio((y))
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
 
-int dat[100005];
-int head = 0, tail = 0;
+int main() {
+    fastio(nullptr, false);
 
-void push(int x){
-	dat[tail++] = x;
-}
-void pop(){
-	if( tail > head ) cout << dat[head++] << '\n';
-	else cout << -1 << '\n';
-}
-int size(){
-	return tail - head;
-}
-int empty(){
-	if( head == tail ) return 1;
-	return 0;
-}
-int front(){
-	if(empty()) return -1;
-	return dat[head];
-}
-int back(){
-	if(empty()) return -1;
-	return dat[tail-1];
-}
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t, elem;
-	string s;
-	cin >> t;
-	while(t--){
-		cin >> s;
-		if( s == "push"){
-			cin >> elem;
-			push(elem);
-		}
-		else if( s == "front") cout << front() << '\n';
-		else if( s == "back" ) cout << back() << '\n';
-		else if( s == "size" ) cout << size() << '\n';
-		else if( s == "empty" ) cout << empty() << '\n';
-		else if( s == "pop") pop();
-	}
+    int N{};
+    cin >> N;
+
+    deque<int> q{};
+    while(N--) {
+        string cmd{};
+        cin >> cmd;
+        if(cmd == "push") {
+            int x{};
+            cin >> x;
+            q.pb(x);
+        } else if(cmd == "pop") {
+            if(sz(q)) {
+                cout << q.front() << nl;
+                q.pop_front();
+            } else {
+                cout << -1 << nl;
+            }
+        } else if (cmd == "size") {
+            cout << sz(q) << nl;
+        } else if(cmd == "empty") {
+            cout << q.empty() << nl;
+        } else if(cmd == "front") {
+            cout << (sz(q) ? q.front() : -1) << nl;
+        } else if(cmd == "back") {
+            cout << (sz(q) ? q.back() : -1) << nl;
+        }
+    }
 }
