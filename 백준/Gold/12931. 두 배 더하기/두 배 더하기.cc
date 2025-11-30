5ace596a30e590242a5a78864c24b90c11cfb148
ChoiJunHyeok <112684671+belight2@users.noreply.github.com>
2025-12-01 01:04:50 +0900

    [Gold V] Title: 두 배 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12931. 두 배 더하기/두 배 더하기.cc
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.cc
@@ -14,76 +14,41 @@ typedef queue<pi> qpi; typedef queue<pl> qpl; typedef queue<ti> qti; typedef que
 #define rall(x) (x).rbegin(), (x).rend()
 const char nl = '\n';
 
-int dp[1005];
-int prv[1005];
-int solve(int x) {
-    if(x <= 0) {
-        return 0;
-    }
-    if(dp[x] != -1) {
-        return dp[x];
-    }
-    dp[x] = solve(x - 1) + 1;
-    prv[x] = 0;
-    if(!(x & 1)) {
-        int k = solve(x / 2) + 1;
-        if(dp[x] > k) {
-            dp[x] = k;
-            prv[x] = 1;
-        }
-    }
+int N;
+vi a;
 
-    return dp[x];
-}
-
-bool chk(vi &x) {
-    for(auto k : x) {
-        if(k) {
-            return 1;
-        }
-    }
-    return 0;
+bool chk() {
+    int x =  count(all(a), 0);
+    return x == N;
 }
 
 int main() {
     fastio(nullptr, false);
-    memset(dp, -1, sizeof(dp));
-    dp[0] = 0;
-    dp[1] = 1;
-    for(int i = 1000; i >= 0; i--) {
-        if(dp[i] == -1) {
-            dp[i] = solve(i);
-        }
-    }
-
-    prv[1] = 0;
-    prv[2] = 1;
-    int N;
+    
     cin >> N;
-    vi a(N);
+    vi(N).swap(a);
     for(auto &x : a) {
         cin >> x;
     }
-
+    
     int ans{};
-    while(chk(a)) {
-        bool isdiv = 1;
-        for(int &x : a) {
+    while(1) {
+        for(auto &x : a) {
             if(x == 0) continue;
-            isdiv &= prv[x];
-
-            if(!prv[x]) {
-                x -= 1;
+            if(x & 1) {
+                x--;
                 ans++;
             }
         }
 
-        if(isdiv) {
-            for(auto &x : a) {
-                x /= 2;
-            }
-            ans++;
+        if(count(all(a), 0) == N) {
+            break;
         }
+
+        for(auto &x : a) {
+            x /= 2;
+        }
+        ans++;
     }
 
     cout << ans;
